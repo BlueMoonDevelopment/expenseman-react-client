57ef15090ac5cc29e0a9d0d4003b8e4f06b1c6fc
TheJeterLP <joey.peter1998@gmail.com>
2023-12-16 22:14:56 +0100

    Update failed.tsx

--- src/pages/auth/failed.tsx
+++ src/pages/auth/failed.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useNavigate} from "react-router-dom";
 
 export default function Failed() {
+    const nav = useNavigate();
+
+    useEffect(()=>{
+        setTimeout(()=> nav('/'),5000);
+    },[])
     return (
         <div className="container has-text-centered">
             <div className="notification is-danger">
-                <p>Error during sign in process! Please try again...</p>
+                <p>Error during sign in process! Please try again.</p>
+                <p>You will be redirected in 5 seconds...</p>
             </div>
         </div>
     );
