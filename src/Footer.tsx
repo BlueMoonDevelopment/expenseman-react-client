7e35a615e8cb0b1c46d28f87c6cd6da071aec742
TheJeterLP <joey.peter1998@gmail.com>
2023-12-18 20:43:29 +0100

    Cleanup

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
                                 <button className="button is-link is-rounded"><span className="icon"><i
                                     className="fab fa-github"></i></span><span>BlueMoonDevelopment</span></button>
                             </a></div>
-                            <div className="column"><a href="https://github.com/BlueMoonDevelopment/expenseman-web"
+                            <div className="column"><a href="https://github.com/BlueMoonDevelopment/expenseman-react-client"
                                                        target="_blank" rel="noreferrer">
                                 <button className="button is-link is-rounded"><span className="icon"><i
                                     className="fab fa-github"></i></span><span>Source Code</span></button>
