49c02a748620d6aaf849ec0112b1fd9545b6fdbc
TheJeterLP <joey.peter1998@gmail.com>
2023-12-18 16:36:45 +0100

    Update SigninButton.tsx

--- src/SigninButton.tsx
+++ src/SigninButton.tsx
@@ -3,49 +3,47 @@ import {API_ENDPOINT_URL, DEVELOPMENT_MODE, GOOGLE_OAUTH_CLIENT_ID} from "./conf
 import {getUrl} from "./tools/Tools";
 import axios from "axios";
 
-export default class SigninButton extends React.Component {
+export default function SigninButton() {
 
-    render() {
-        axios.get(getUrl('/auth/checksignedin'), {
-            withCredentials: true,
-            validateStatus: function (status) {
-                return status < 500;
-            }
-        }).then(res => {
-            const status = res.status;
-            console.log('Status: ' + status);
-            if (status === 401) {
-                console.log('Message: ' + res.data.message);
-                console.log('Rendering sign-in button');
-                return (
-                    <>
-                        <div id="g_id_onload"
-                             data-client_id={GOOGLE_OAUTH_CLIENT_ID}
-                             data-context="signin"
-                             data-ux_mode="popup"
-                             data-login_uri={(DEVELOPMENT_MODE ? "http://localhost:8082" : API_ENDPOINT_URL) + "/auth/google"}
-                             data-auto_select="true"
-                             data-itp_support="true"
-                        >
-                        </div>
-                        <div className="g_id_signin"
-                             data-type="standard"
-                             data-shape="pill"
-                             data-theme="outline"
-                             data-text="signin_with"
-                             data-size="large"
-                             data-logo_alignment="left"
-                        >
-                        </div>
-                    </>
-                );
-            } else {
-                console.log('NOT rendering sign-in button')
-                return (
-                    <></>
-                )
-            }
-        })
-        return <></>
-    }
+    axios.get(getUrl('/auth/checksignedin'), {
+        withCredentials: true,
+        validateStatus: function (status) {
+            return status < 500;
+        }
+    }).then(res => {
+        const status = res.status;
+        console.log('Status: ' + status);
+        if (status === 401) {
+            console.log('Message: ' + res.data.message);
+            console.log('Rendering sign-in button');
+            return (
+                <>
+                    <div id="g_id_onload"
+                         data-client_id={GOOGLE_OAUTH_CLIENT_ID}
+                         data-context="signin"
+                         data-ux_mode="popup"
+                         data-login_uri={(DEVELOPMENT_MODE ? "http://localhost:8082" : API_ENDPOINT_URL) + "/auth/google"}
+                         data-auto_select="true"
+                         data-itp_support="true"
+                    >
+                    </div>
+                    <div className="g_id_signin"
+                         data-type="standard"
+                         data-shape="pill"
+                         data-theme="outline"
+                         data-text="signin_with"
+                         data-size="large"
+                         data-logo_alignment="left"
+                    >
+                    </div>
+                </>
+            );
+        } else {
+            console.log('NOT rendering sign-in button')
+            return (
+                <></>
+            )
+        }
+    })
+    return <></>
 }
